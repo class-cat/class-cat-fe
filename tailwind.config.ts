08eaf38f9ac21c88a42e2c5afd47060171a11264
Szymon Sadowski <szymon.sadowski@lingarogroup.com>
2024-05-13 17:27:05 +0200

    feat: theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      primary: "#E74C3C",
+      secondary: "#F4ECDF",
+      white: "#FFFEFB",
+      black: "#1C1E31",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
