878f1df4ef87ac9a9d415a6b77da0aa23e3d7748
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2024-07-03 23:08:26 +0200

    feat: add actions for search page

---
+++ src/actions/get-locations.tsx
@@ -0,0 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import { Location } from "~/types/search.type";
+
+const locations: Location[] = [
+  {
+    value: "bydgoszcz",
+    label: "Bydgoszcz",
+  },
+  {
+    value: "gdansk",
+    label: "Gdańsk",
+  },
+  {
+    value: "poznan",
+    label: "Poznań",
+  },
+  {
+    value: "warszawa",
+    label: "Warszawa",
+  },
+  {
+    value: "krakow",
+    label: "Kraków",
+  },
+  {
+    value: "wroclaw",
+    label: "Wrocław",
+  },
+  {
+    value: "gdynia",
+    label: "Gdynia",
+  },
+]
+
+const getLocations = async (): Promise<Location[]> => {
+    return locations
+};
+
+export const useGetLocations = () => {
+  return useQuery({
+    queryKey: ["locations-data"],
+    queryFn: () => getLocations(),
+  });
+};
--- src/types/lesson.type.ts
+++
@@ -1,7 +0,0 @@
-export type Lesson = {
-    name: string;
-    title: string;
-    address: string;
-    avatar: string;
-  }
-  
