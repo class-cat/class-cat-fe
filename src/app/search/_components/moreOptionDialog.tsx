97eab9c1532ac9cec3654dc70710bb8501882854
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2024-07-10 19:47:06 +0200

    feat: add price slider

--- src/app/search/_components/moreOptionDialog.tsx
+++ src/app/search/_components/moreOptionDialog.tsx
@@ -6,44 +6,48 @@ import { Button } from "~/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select"
 import { useUpdateQueryParams } from "~/hooks/useUpdateQueryParams"
+import { PriceSlider } from "./priceSlider"
 
 const distanceData = [
-    {value: '0', label: "+0 km"},
-    {value: '5', label: "+5 km"},
-    {value: '10', label: "+10 km"},
-    {value: '15', label: "+15 km"},
-    {value: '20', label: "+20 km"},
-    {value: '30', label: "+30 km"},
-    {value: '50', label: "+50 km"},
+    { value: '0', label: "+0 km" },
+    { value: '5', label: "+5 km" },
+    { value: '10', label: "+10 km" },
+    { value: '15', label: "+15 km" },
+    { value: '20', label: "+20 km" },
+    { value: '30', label: "+30 km" },
+    { value: '50', label: "+50 km" },
 ]
 
 const classRangeData = [
-    {value: '1', label: "1-3"},
-    {value: '2', label: "4-6"},
-    {value: '3', label: "7-8"},
-    {value: '4', label: "ponad podstawowa"},
+    { value: '1', label: "1-3" },
+    { value: '2', label: "4-6" },
+    { value: '3', label: "7-8" },
+    { value: '4', label: "ponad podstawowa" },
 ]
 
 type Props = {
     distanceValue: string | null
     ageValue: string | null
+    priceValue: string | null
 }
 
-export const MoreOptionDialog = ({distanceValue, ageValue} : Props) => {
+export const MoreOptionDialog = ({ distanceValue, ageValue, priceValue }: Props) => {
     const updateQueryParams = useUpdateQueryParams()
 
     const [open, setOpen] = useState(false)
     const [distance, setDistance] = useState(distanceValue)
     const [age, setAge] = useState(ageValue)
+    const [price, setPrice] = useState<number[] | undefined>(priceValue ? [parseInt(priceValue, 10)] : [0])
 
     const handleSaveOptions = () => {
-        updateQueryParams({ distance: distance, age: age });
+        updateQueryParams({ distance, age, price: price ? price.toString() : null });
         setOpen(false)
     };
 
     const handleResetOptions = () => {
         setDistance(null)
         setAge(null)
+        setPrice(undefined)
     };
 
     const handleOpenChange = (isOpen: boolean) => {
@@ -51,12 +55,13 @@ export const MoreOptionDialog = ({distanceValue, ageValue} : Props) => {
         if (isOpen) {
             setDistance(distanceValue)
             setAge(ageValue)
+            setPrice(priceValue ? [parseInt(priceValue, 10)] : [0])
         }
     };
-    
-    return( 
+
+    return (
         <Dialog open={open} onOpenChange={handleOpenChange}>
-              <DialogTrigger className="flex w-full justify-center md:justify-between" asChild> 
+            <DialogTrigger className="flex w-full justify-center md:justify-between" asChild>
                 <Button
                     variant="ghost"
                     className="rounded-lg border-2 border-secondary py-5 shadow-none hover:bg-secondary"
@@ -68,56 +73,61 @@ export const MoreOptionDialog = ({distanceValue, ageValue} : Props) => {
                 </Button>
             </DialogTrigger>
             <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Więcej opcji</DialogTitle>
-                <DialogDescription>
-                    <div className="flex flex-col gap-1">
-                       <div className="text-lg">
-                        Odległość
-                        </div>
-                        <div>
-                            <Select value={distance || undefined} onValueChange={setDistance}>
-                                <SelectTrigger className="w-[210px]">
-                                    <SelectValue placeholder="Odległość" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {distanceData.map((item) => (
-                                        <SelectItem 
-                                            key={item.value} 
-                                            value={item.value}
-                                        >
-                                            {item.label}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
+                <DialogHeader>
+                    <DialogTitle>Więcej opcji</DialogTitle>
+                    <DialogDescription>
+                        <div className="flex flex-col gap-1">
+                            <div className="text-lg">
+                                Odległość
+                            </div>
+                            <div>
+                                <Select value={distance || undefined} onValueChange={setDistance}>
+                                    <SelectTrigger className="w-[210px]">
+                                        <SelectValue placeholder="Odległość" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {distanceData.map((item) => (
+                                            <SelectItem
+                                                key={item.value}
+                                                value={item.value}
+                                            >
+                                                {item.label}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
                                 </Select>
+                            </div>
+                            <div className="text-lg mt-2">
+                                Klasa
+                            </div>
+                            <div>
+                                <Select value={age || undefined} onValueChange={setAge}>
+                                    <SelectTrigger className="w-[210px]">
+                                        <SelectValue placeholder="Klasa" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {classRangeData.map((item) => (
+                                            <SelectItem key={item.value} value={item.value}>
+                                                {item.label}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="text-lg mt-2">
+                                Kwota
+                            </div>
+                            <div>
+                                <PriceSlider value={price} onValueChange={setPrice} />
+                            </div>
                         </div>
-                        <div className="text-lg mt-2">
-                        Klasa
-                        </div>
-                        <div>
-                            <Select value={age || undefined} onValueChange={setAge}>
-                                <SelectTrigger className="w-[210px]">
-                                    <SelectValue placeholder="Klasa" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {classRangeData.map((item) => (
-                                        <SelectItem key={item.value} value={item.value}>
-                                            {item.label}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                    </div>
-                </DialogDescription>
-            </DialogHeader>
-            <DialogFooter className="mt-2">
-                <Button variant="outline" onClick={() => handleResetOptions()}>Wyczyść</Button>
-                <Button onClick={() => handleSaveOptions()}>Zapisz</Button>
-
-            </DialogFooter>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="mt-2">
+                    <Button variant="outline" onClick={() => handleResetOptions()}>Wyczyść</Button>
+                    <Button onClick={() => handleSaveOptions()}>Zapisz</Button>
+                </DialogFooter>
             </DialogContent>
-        </Dialog>        
+        </Dialog>
     )
 }
