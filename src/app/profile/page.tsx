b05fb9eea11f36031bf3673797bff851fa59227f
Szymon Sadowski <szymon.sadowski@lingarogroup.com>
2024-07-11 12:08:04 +0200

    feat: profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import { Container } from "~/components/ui/container"
 import { Input } from "~/components/ui/input"
+<<<<<<< HEAD
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
 import { z } from "zod"
@@ -38,6 +39,35 @@ import {
 import { Checkbox } from "~/components/ui/checkbox"
 import { toast } from "sonner"
 
+=======
+import { Label } from "~/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { useForm } from "react-hook-form"
+import { toast } from "~/components/ui/use-toast"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form"
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover"
+
+import { Button } from "~/components/ui/button"
+import { cn } from "~/lib/utils"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "~/components/ui/calendar"
+import { format } from "date-fns"
+type TabsTriggers = "lessons" | "reviews" | "school" | "settings"
+>>>>>>> 72a9a3d (feat: profile page)
 const tabsTriggers = [
   {
     id: 1,
@@ -62,22 +92,41 @@ const tabsTriggers = [
 ]
 
 const FormSchema = z.object({
+<<<<<<< HEAD
   dob: z.date().optional(),
   address: z.string().min(2).max(50).optional(),
   sex: z.enum(["male", "female", "unknown"]).optional(),
   email: z.boolean().optional(),
   sms: z.boolean().optional(),
+=======
+  dob: z.date(),
+>>>>>>> 72a9a3d (feat: profile page)
 })
 type FormSchemaType = z.infer<typeof FormSchema>
 
 const ProfileTabsContent = () => {
   const form = useForm<FormSchemaType>({
+<<<<<<< HEAD
+=======
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+>>>>>>> 72a9a3d (feat: profile page)
     resolver: zodResolver(FormSchema),
   })
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
+<<<<<<< HEAD
     console.log(data)
     toast("Form submitted")
+=======
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="bg-slate-950 mt-2 w-[340px] rounded-md p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+>>>>>>> 72a9a3d (feat: profile page)
   }
 
   return (
@@ -93,9 +142,19 @@ const ProfileTabsContent = () => {
           <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">test</div>
         </TabsContent>
         <TabsContent value="settings" className="card">
+<<<<<<< HEAD
           <div className="flex flex-col gap-8 md:flex-row md:justify-between">
             <div className="flex min-w-[240px] flex-col gap-4">
               <h2 className="text-xl font-bold">Dodatkowe Informacje</h2>
+=======
+          <div className="flex flex-col md:flex-row md:justify-between">
+            <div className="flex flex-col gap-4">
+              <h2 className="text-xl font-bold">Dodatkowe Informacje</h2>
+              {/* <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="bday">Data urodzenia</Label>
+                <Input type="date" id="bday" placeholder="Data urodzenia" />
+              </div> */}
+>>>>>>> 72a9a3d (feat: profile page)
               <FormField
                 control={form.control}
                 name="dob"
@@ -108,7 +167,11 @@ const ProfileTabsContent = () => {
                           <Button
                             variant={"outline"}
                             className={cn(
+<<<<<<< HEAD
                               "w-full border-0 pl-3 text-left font-normal",
+=======
+                              "w-[240px] pl-3 text-left font-normal",
+>>>>>>> 72a9a3d (feat: profile page)
                               !field.value && "text-muted-foreground"
                             )}
                           >
@@ -143,6 +206,7 @@ const ProfileTabsContent = () => {
                   </FormItem>
                 )}
               />
+<<<<<<< HEAD
               <FormField
                 control={form.control}
                 name="address"
@@ -226,6 +290,11 @@ const ProfileTabsContent = () => {
                 />
               </div>
               <Button type="submit">Zapisz</Button>
+=======
+            </div>
+            <div>
+              <h2 className="text-xl font-bold">Powiadomienia</h2>
+>>>>>>> 72a9a3d (feat: profile page)
             </div>
           </div>
         </TabsContent>
