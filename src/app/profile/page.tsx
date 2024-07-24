d96805d289c28c165820a83521c480abf74bac14
Szymon Sadowski <szymon.sadowski@lingarogroup.com>
2024-07-24 14:54:30 +0200

    feat: new profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -37,6 +37,9 @@ import {
 } from "~/components/ui/select"
 import { Checkbox } from "~/components/ui/checkbox"
 import { toast } from "sonner"
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
+import { useUser } from "@clerk/nextjs"
+import { Skeleton } from "~/components/ui/skeleton"
 
 const tabsTriggers = [
   {
@@ -61,21 +64,18 @@ const tabsTriggers = [
   },
 ]
 
-const FormSchema = z.object({
-  dob: z.date().optional(),
-  address: z.string().min(2).max(50).optional(),
-  sex: z.enum(["male", "female", "unknown"]).optional(),
+const FormNotificationsSchema = z.object({
   email: z.boolean().optional(),
   sms: z.boolean().optional(),
 })
-type FormSchemaType = z.infer<typeof FormSchema>
+type FormNotificationsSchemaType = z.infer<typeof FormNotificationsSchema>
 
 const ProfileTabsContent = () => {
-  const form = useForm<FormSchemaType>({
-    resolver: zodResolver(FormSchema),
+  const form = useForm<FormNotificationsSchemaType>({
+    resolver: zodResolver(FormNotificationsSchema),
   })
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
+  function onSubmit(data: z.infer<typeof FormNotificationsSchema>) {
     console.log(data)
     toast("Form submitted")
   }
@@ -93,100 +93,7 @@ const ProfileTabsContent = () => {
           <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">test</div>
         </TabsContent>
         <TabsContent value="settings" className="card">
-          <div className="flex flex-col gap-8 md:flex-row md:justify-between">
-            <div className="flex min-w-[240px] flex-col gap-4">
-              <h2 className="text-xl font-bold">Dodatkowe Informacje</h2>
-              <FormField
-                control={form.control}
-                name="dob"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Data urodzenia</FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant={"outline"}
-                            className={cn(
-                              "w-full border-0 pl-3 text-left font-normal",
-                              !field.value && "text-muted-foreground"
-                            )}
-                          >
-                            {field.value ? (
-                              format(field.value, "PPP")
-                            ) : (
-                              <span>Wybierz datę</span>
-                            )}
-                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent
-                        className="w-auto rounded-md bg-white p-0 shadow-xl"
-                        align="start"
-                      >
-                        <Calendar
-                          mode="single"
-                          captionLayout="dropdown-buttons"
-                          selected={field.value}
-                          onSelect={field.onChange}
-                          disabled={(date) =>
-                            date > new Date() || date < new Date("1900-01-01")
-                          }
-                          initialFocus
-                          fromYear={1960}
-                          toYear={2030}
-                        />
-                      </PopoverContent>
-                    </Popover>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="address"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Adres zamieszkania</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Adres zamieszkania"
-                        className="w-full"
-                        {...field}
-                      />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="sex"
-                render={({ field }) => (
-                  <FormItem className="space-y-0">
-                    <FormLabel>Płeć</FormLabel>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="h-10 rounded-xl bg-white shadow-md">
-                          <SelectValue placeholder="Wybierz płeć" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="female">Kobieta</SelectItem>
-                        <SelectItem value="male">Mężczyzna</SelectItem>
-                        <SelectItem value="unknown">Nie wiem</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+          <div className="flex flex-col gap-8">
             <div className="flex min-w-[240px] flex-col justify-between">
               <div className="flex flex-col gap-4">
                 <h2 className="text-xl font-bold">Powiadomienia</h2>
@@ -224,8 +131,24 @@ const ProfileTabsContent = () => {
                     </FormItem>
                   )}
                 />
+                <div className="h-2" />
+                <Button type="submit">Zapisz</Button>
               </div>
-              <Button type="submit">Zapisz</Button>
+            </div>
+            <div className="flex flex-col gap-4">
+              <h2 className="text-xl font-bold">Polityka Prywatności</h2>
+              <Button className="w-full bg-white" variant={"link"}>
+                Umowy o poufności
+              </Button>
+              <Button className="w-full bg-white" variant={"link"}>
+                Prawa wnioski/RODO
+              </Button>
+            </div>
+            <div className="flex flex-col gap-4">
+              <h2 className="text-xl font-bold">Konto</h2>
+              <Button className="bg-outline" variant={"combobox"} disabled>
+                Usuń konto
+              </Button>
             </div>
           </div>
         </TabsContent>
@@ -233,7 +156,29 @@ const ProfileTabsContent = () => {
     </Form>
   )
 }
+
+const FormSchema = z.object({
+  firstName: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
+  dob: z.date().optional(),
+  address: z.string().min(2).max(50).optional(),
+  sex: z.enum(["male", "female", "unknown"]).optional(),
+  emailAddress: z.string().email("This is not a valid email."),
+})
+type FormSchemaType = z.infer<typeof FormSchema>
+
 export default function ProfilePage() {
+  const { user, isLoaded } = useUser()
+  console.log(user?.primaryEmailAddress?.emailAddress)
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data)
+    toast("Form submitted")
+  }
+
   return (
     <Container className="min-h-screen">
       <div className="flex flex-col gap-8 pt-6 md:flex-row md:justify-center">
@@ -260,7 +205,180 @@ export default function ProfilePage() {
             <ProfileTabsContent />
           </Tabs>
         </section>
-        <section className="card md:w-1/3"></section>
+        {!isLoaded ? (
+          <Skeleton className="card min-h-[500px] rounded-3xl md:w-1/3" />
+        ) : (
+          <section className="cardSmall md:w-1/3">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <div className="flex min-w-[240px] flex-col gap-4">
+                  <Avatar className="mx-auto h-48 w-48">
+                    <AvatarImage src={user?.imageUrl} />
+                    <AvatarFallback>User</AvatarFallback>
+                  </Avatar>
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    defaultValue={user?.firstName || ""}
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Imie</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Imie"
+                            className="w-full"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="lastName"
+                    defaultValue={user?.lastName || ""}
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Nazwisko</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Nazwisko"
+                            className="w-full"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="emailAddress"
+                    defaultValue={user?.primaryEmailAddress?.emailAddress || ""}
+                    disabled={
+                      user?.primaryEmailAddress?.emailAddress ? true : false
+                    }
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Email"
+                            className="w-full"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="dob"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Data urodzenia</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-full border-0 pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Wybierz datę</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent
+                            className="w-auto rounded-md bg-white p-0 shadow-xl"
+                            align="start"
+                          >
+                            <Calendar
+                              mode="single"
+                              captionLayout="dropdown-buttons"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) =>
+                                date > new Date() ||
+                                date < new Date("1900-01-01")
+                              }
+                              initialFocus
+                              fromYear={1960}
+                              toYear={2030}
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="address"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Adres zamieszkania</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Adres zamieszkania"
+                            className="w-full"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="sex"
+                    render={({ field }) => (
+                      <FormItem className="space-y-0">
+                        <FormLabel>Płeć</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="h-10 rounded-xl bg-white shadow-md">
+                              <SelectValue placeholder="Wybierz płeć" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="female">Kobieta</SelectItem>
+                            <SelectItem value="male">Mężczyzna</SelectItem>
+                            <SelectItem value="unknown">Nie wiem</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="h-8" />
+                <Button type="submit" className="w-full">
+                  Zapisz
+                </Button>
+              </form>
+            </Form>
+          </section>
+        )}
       </div>
       <div className="h-16" />
     </Container>
