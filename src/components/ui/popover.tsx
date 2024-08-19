7e57c061b4d546f484a4c84329c4b1f972ca566b
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2024-08-20 00:02:11 +0200

    feat: update popover

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -12,10 +12,10 @@ const PopoverTrigger = PopoverPrimitive.Trigger
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align, sideOffset = 4, ...props }, ref) => (
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Content
     ref={ref}
-    align={align || 'center'}
+    align={align}
     sideOffset={sideOffset}
     className={cn(
       "z-50 w-72 rounded-md border-2 border-primary bg-popover p-4 text-foreground shadow-md outline-none",
