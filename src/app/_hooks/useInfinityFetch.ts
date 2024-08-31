9dc9ee6b41c4b96725e1745b368e2ab681e36f90
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-08-31 18:09:17 +0200

    fix: types

--- src/app/_hooks/useInfinityFetch.ts
+++ src/app/_hooks/useInfinityFetch.ts
@@ -1,17 +1,16 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { type FetchConfig, type QueryKeyT, fetcher } from "~/lib/query-client";
+import { useInfiniteQuery, QueryKey, InfiniteData, UseInfiniteQueryOptions } from "@tanstack/react-query";
+import { type FetchConfig, fetcher } from "~/lib/query-client";
 
 type UseFetch<T> = {
   url: string | null;
   params?: Record<string, string | number>;
-  config?: FetchConfig<T>;
+  config?: Omit<UseInfiniteQueryOptions<PageData<T>, Error, InfiniteData<PageData<T>>, PageData<T>, QueryKey>, 'queryKey' | 'queryFn' | 'initialPageParam' | 'getNextPageParam'>;
   enabled?: boolean;
 };
 
 interface PageData<T> {
-  data: {
-    next: boolean;
-  };
+  data: any;
+  next: boolean;
 }
 
 export const useInfinityFetch = <T>({
@@ -20,28 +19,29 @@ export const useInfinityFetch = <T>({
   config,
   enabled = !!url,
 }: UseFetch<T>) => {
-  const queryKey: QueryKeyT = [url as string, params as Record<string, string | number>];
+  const queryKey: QueryKey = [url as string, params];
 
-  return useInfiniteQuery<PageData<T>, Error>({
+  return useInfiniteQuery<PageData<T>, Error, InfiniteData<PageData<T>>>({
     queryKey,
     initialPageParam: 1,
+    
     queryFn: ({ pageParam = 1, signal }) => {
       if (!url) throw new Error("URL is required");
       const queryParams = { page: pageParam as string, ...params };
-      return fetcher({
+      return fetcher<PageData<T>>({
         queryKey: [url, queryParams],
         signal,
+        meta: {
+          persist: false
+        }
       });
     },
-    enabled: enabled,
+    enabled,
     getNextPageParam: (lastPage, allPages) => {
       console.log(allPages);
-      return lastPage?.data.next ? allPages.length + 1 : undefined;
+      return lastPage?.data?.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 1000 * 60 * 5,
-    cacheTime: 1000 * 60 * 10,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
+    
     ...config,
   });
 };
