83940623a63340b2d172e8204207cc29de596f52
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2024-08-31 19:59:36 +0200

    feat: format card

--- src/components/pill/pill.tsx
+++ src/components/pill/pill.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import Image from "next/image"
 import { useState } from "react"
 import { useMediaQuery } from "~/app/_hooks/useMediaQuery"
@@ -11,7 +12,6 @@ import {
   TooltipTrigger,
 } from "../ui/tooltip"
 import { IconWithText } from "../ui/icon-text"
-// import { Activity } from "~/types/search.type"
 
 export function Pill(props: any) {
   const { name, location, provider, primary_image } = props
@@ -24,8 +24,8 @@ export function Pill(props: any) {
   }
 
   return (
-    <div className="relative mt-2 flex cursor-pointer items-center gap-4 rounded-2xl border-2 border-secondary p-2 hover:shadow-md">
-      <div className="relative size-[50px] sm:size-[80px]">
+    <div className="relative mt-2 flex cursor-pointer items-center gap-2 rounded-2xl border-2 border-secondary p-2 hover:shadow-md sm:gap-4">
+      <div className="relative size-[50px] shrink-0 sm:size-[80px]">
         {primary_image?.file ? (
           <Image
             src={primary_image?.file}
@@ -35,38 +35,33 @@ export function Pill(props: any) {
             className="rounded-lg"
           />
         ) : (
-          <Icons.placeholder />
+          <Icons.placeholder className="size-full" />
         )}
       </div>
-      <div>
-        <p className="font-medium">{name || "Siatkówka dla klas 1-3"}</p>
-        {name && !isMobile && (
+      <div className="flex min-w-0 grow flex-col">
+        <p className="truncate text-sm  font-medium sm:text-base">
+          {name || "Siatkówka dla klas 1-3"}
+        </p>
+        {!isMobile && (
           <IconWithText text={location?.address?.addressLine}>
-            <Icons.map />
+            <Icons.map className="size-4 shrink-0" />
           </IconWithText>
         )}
-        {
-          <IconWithText text={provider?.name}>
-            <Icons.store />
-          </IconWithText>
-        }
+        <IconWithText text={provider?.name}>
+          <Icons.store className="size-4 shrink-0" />
+        </IconWithText>
       </div>
-      {/* <button onClick={handleBookmark}>
-        <Icons.badge
-          className="absolute right-[10px] top-[-15px] size-8"
-          color={bookmark ? "#E74C3C" : "#FFFEFB"}
-        />{" "}
-      </button> */}
-      <div className="absolute right-[10px] top-[5px] ">
-        <TooltipProvider>
+      <div className="size-10"/>
+      <div className="absolute right-2 top-2">
+      <TooltipProvider>
           <Tooltip>
             <TooltipTrigger onClick={handleBookmark}>
               <Icons.paw
-                className="size-8"
+                className="size-6 sm:size-8"
                 color={"#ecdec8"}
                 fill={bookmark ? "#ecdec8" : "#FFFEFB"}
                 strokeWidth={1.5}
-              />{" "}
+              />
             </TooltipTrigger>
             <TooltipContent>
               {bookmark ? "Usuń z ulubionych" : "Dodaj do ulubionych"}
@@ -74,6 +69,9 @@ export function Pill(props: any) {
           </Tooltip>
         </TooltipProvider>
       </div>
+     
+       
+      
     </div>
   )
 }
--- src/components/ui/icon-text.tsx
+++ src/components/ui/icon-text.tsx
@@ -8,7 +8,7 @@ export function IconWithText({ text, children }: IconWithTextProps) {
   return (
     <div className="flex flex-row items-center gap-1 pt-1">
       {children}
-      <p className="text-xs text-foregroundMuted">{text}</p>
+      <p className="ml-1 truncate text-xs text-foregroundMuted ">{text}</p>
     </div>
   )
 }
