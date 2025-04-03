efd2623aefbee15b759d8703bab59668714352fb
Wicki Krzysztof <krzysztof_wicki@sevenet.pl>
2025-04-03 21:09:58 +0200

    feat: update map

--- src/app/_components/map/mobileMap.tsx
+++ src/app/_components/map/mobileMap.tsx
@@ -16,29 +16,30 @@ export function MobileMap() {
   const handleOpenChange = (isOpen: boolean) => {
     setOpen(isOpen)
   }
+
   return (
-    <>
-      <Dialog open={open} onOpenChange={handleOpenChange}>
-        <DialogTrigger asChild>
-          <div className="h-[80px] w-full rounded-2xl bg-[url('/map-button-light.png')]">
-            <div className="flex size-full items-center justify-center rounded-2xl">
-              <Button
-                variant="outline"
-                className="w-[180px] border-0"
-                onClick={openMapDialog}
-              >
-                <Icons.globe className="size-5" />
-                <span className="ml-2">Wyświetl mapę</span>
-              </Button>
-            </div>
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogTrigger asChild>
+        <div className="h-[80px] w-full rounded-2xl bg-[url('/map-button-light.png')]">
+          <div className="flex size-full items-center justify-center rounded-2xl">
+            <Button
+              variant="outline"
+              className="w-[180px] border-0"
+              onClick={openMapDialog}
+            >
+              <Icons.globe className="size-5" />
+              <span className="ml-2">Wyświetl mapę</span>
+            </Button>
           </div>
-        </DialogTrigger>
-        <DialogContent className="top-[1/2] h-screen w-screen px-2 pt-10">
-          <div className="p-1">
+        </div>
+      </DialogTrigger>
+      <DialogContent className="w-full p-0 sm:h-[80vh] md:max-w-[90vw]">
+        <div className="relative size-full">
+          <div className="size-full">
             <Map />
           </div>
-        </DialogContent>
-      </Dialog>
-    </>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
